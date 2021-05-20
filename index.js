b291b40abd4e00f92af0a5aa67e57ee164b813b9
Sajeyks <sajeyksmwangi@gmail.com>
2021-05-20 18:41:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Web");
+document.write("Welcome to Web");
+//var name = "Njoki";
+
+function changeName() {
+    document.getElementById("name").innerHTML = "NJOKI";
+
+    var x = document.getElementsByClassName("Age");
+    x[0].innerHTML = "23 Years";
+
+}
